9a5e818058ea7e99f5397326a5610e0cc8be0a28
remster85 <remi.desreumaux@gmail.com>
2017-07-24 21:01:33 -0400

    Create VUE.js

---
+++ VUE.js
@@ -0,0 +1,59 @@
+Discover Vue.js
+7/24/2017
+New York City
+I Heart Radio office
+
+What is a Vue?
+Vue is a very lightweight framework.
+Syntax similar than AngularKS
+Has two-way data flow between
+Conditionals and loops v-if, v-show, v-on
+two=way bindings
+npm install -g 
+
+Kick off VueNYC August 22th 
+http://vuejs.org/v2/guide/
+https://github.com/designMoreWeb
+https://scotch.io/tutorials/the-ultimate-guide-to-progressive-web-applications
+
+Talk #2
+https://github.com/designMoreWeb
+https://designmoreweb.github.io/
+
+Server Side Rendering Async Components
+Source code Light
+Simple to use
+Readibility and maintainibility 
+best of angular and react
+
+Limitations
+API might change
+Lack of testing tools
+Dev communauty is chinese
+Hyperscript is an alternative of TypeScript
+
+Talk #3
+Vue Components
+andregarvin working for openfin, startup for investment bank and hedge funds.
+dropdown, postcard, navigationbar.
+
+openkey.herokuapp.com
+demo profile page of a person with hobbies.
+Old way is to create a template with the person logic
+New way is to create a vue component <peopleinfocard></peopleinfocard>
+Props is data or methods that can be transfered to components
+
+Live coding Demo
+
+Talk #4
+Hyperscript
+@kirjs
+https://github.com/hyperhype/hyperscript/blob/master/examples/index.html
+
+Talk #5
+Vue CLI
+neural network can name guinea pigs
+
+
+
+ 
